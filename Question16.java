435f51f8c0421ea186dd7aa70f3358743a553f08
HrishikeshPawar <128132718+HrishikeshPawar@users.noreply.github.com>
2023-03-28 11:45:55 +0530

    java Program uploded

---
+++ Question16.java
@@ -0,0 +1,13 @@
+package Assignment2;
+import java.util.*;
+public class Question16 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.print("Input first number: ");
+       double n1 = sc.nextDouble();
+       System.out.print("Input second number: ");
+       double n2 = sc.nextDouble();
+		 System.out.println(n1 > 0 && n1 < 1 && n2 > 0 && n2 < 1);
+
+	}
+}
